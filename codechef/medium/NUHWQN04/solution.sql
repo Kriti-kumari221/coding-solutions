b57cdf8b822a4c8c80e613633f6db4a8eca9365f
kriti <151952844+Kriti-kumari221@users.noreply.github.com>
2026-09-05 10:51:47 +0530

    ✅ NUHWQN04 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-09-05

---
+++ codechef/medium/NUHWQN04/solution.sql
@@ -0,0 +1,5 @@
+-- Type each SQL query on a new line.
+-- Your code here..
+select distinct species from Trees;
+select * from Trees where ward=14 or ward=20;
+select * from trees order by planted_date asc;
