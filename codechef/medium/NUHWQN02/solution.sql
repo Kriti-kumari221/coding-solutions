4d8475cecc4b1c1db3ecae0454d5a70766402548
kriti <151952844+Kriti-kumari221@users.noreply.github.com>
2026-09-05 10:36:46 +0530

    ✅ NUHWQN02 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-09-05

---
+++ codechef/medium/NUHWQN02/solution.sql
@@ -0,0 +1,6 @@
+-- Type each SQL query on a new line.
+-- Your code here..
+select * from City_events where location="Delhi";
+select * from City_events where event_name like "Tech%";
+select * from City_events order by event_date desc;
+
