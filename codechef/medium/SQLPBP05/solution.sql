fd64971edf6734c71eaaedb76e6ed9d29cf6cbfc
kriti <151952844+Kriti-kumari221@users.noreply.github.com>
2026-09-05 10:07:43 +0530

    ✅ SQLPBP05 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-09-05

--- codechef/medium/SQLPBP05/solution.sql
+++ codechef/medium/SQLPBP05/solution.sql
@@ -1,2 +1,2 @@
 -- your code goes here
-select m.match_id, m.player_1, m.player_2, m.winner, m.match_date, p.score from Matches m join players p on m.winner=p.player_name order by m.match_date desc limit 5  
+select m.match_id, m.player_1, m.player_2, m.winner, m.match_date, p.score from Matches m join players p on p.player_name=m.winner order by m.match_date desc limit 5  
