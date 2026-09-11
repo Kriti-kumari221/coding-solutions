75ba9e9f18770fa4e04b8c4a51a188b1812b0cbc
kriti <151952844+Kriti-kumari221@users.noreply.github.com>
2026-09-11 22:55:33 +0530

    Solved: Maximum Subarray (leetcode)

--- leetcode/Medium/maximum-subarray.java
+++ leetcode/Medium/maximum-subarray.java
@@ -4,20 +4,18 @@
 // Language: java
 // Verdict: Accepted
 // URL: https://leetcode.com/problems/maximum-subarray/
-// Solved on: 2026-09-11T17:16:21.176Z
+// Solved on: 2026-09-11T17:25:31.139Z
 
 class Solution {
     public int maxSubArray(int[] nums) {
-        int n=nums.length;
         int sum=0;
         int max=nums[0];
-        for(int i=0;i<n;i++){
+        for(int i=0;i<nums.length;i++){
             sum=Math.max(nums[i],sum+nums[i]);
             if(max<sum){
                 max=sum;
             }
         }
         return max;
-        
     }
 }
