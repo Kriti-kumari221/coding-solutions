274e1313a6862feab1e8b63455649db862f45980
kriti <151952844+Kriti-kumari221@users.noreply.github.com>
2026-09-11 23:03:25 +0530

    ✅ Maximum Product Subarray | Medium | Java | Runtime: 0 ms | Memory: 42.8 MB | 2026-09-11

--- leetcode/medium/0152-maximum-product-subarray/solution.java
+++ leetcode/medium/0152-maximum-product-subarray/solution.java
@@ -3,7 +3,7 @@ class Solution {
         int pro=1;
         int max=nums[0];
         for(int i=0;i<nums.length;i++){
-            pro=Math.max(nums[i],pro*nums[i]);
+            pro=pro*nums[i];
             if(max<pro){
                 max=pro;
             }
